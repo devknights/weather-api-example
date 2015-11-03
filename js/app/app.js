c542af4315be9a7db9d16b14d74e7085f92c3c24
Damian Whyte <damiangw27@gmail.com>
2015-11-03 21:12:17 +0000

    Add logic to display temperature in celcius

--- js/app/app.js
+++ js/app/app.js
@@ -4,17 +4,25 @@
 
     var _atomic;
     var _weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=16e48e710562389863f4671cb6166d9f';
+    var _temperatureDisplayEl;
 
     function init () {
 
-        console.log('init');
-        
+        _temperatureDisplayEl = document.querySelector('.temperature-display');
+
         _atomic = window.atomic;
         _atomic.get(_weatherEndpoint).success(dataReceived).error(dataError);
+
     }
 
     function dataReceived (data) {
-        console.log('dataReceived:', data);
+        console.log(kelvinToCelcius(data.main.temp));
+        _temperatureDisplayEl.innerHTML = kelvinToCelcius(data.main.temp);
+    }
+
+    function kelvinToCelcius (kelvin) {
+        var zeroCelciusInKelvin = 273.15;
+        return Math.round((kelvin - zeroCelciusInKelvin) * 10) / 10;
     }
 
     function dataError (data) {
